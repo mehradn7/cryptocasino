5c3a1fb1799bedfbc7d8250e41ae723d39ea6f6e
duchamvi <vincent.duchamp91@gmail.com>
2019-02-26 15:10:10 +0100

    corrected mistake in the lfsr

--- rng/lfsr.py
+++ rng/lfsr.py
@@ -7,8 +7,9 @@ class Lfsr:
     def nextBit(self):
         next = (self.state[0] + self.state[1] + self.state[3]+ self.state[4])%2
         self.state = [next] + self.state
+        lost = self.state[-1]
         self.state = self.state[:-1]
-        return next
+        return lost
 
     def getRandomNumber(self):
         # 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1
