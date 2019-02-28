7555b6deff4e6897901edcae7690d0935bdfef1f
duchamvi <vincent.duchamp91@gmail.com>
2019-02-28 13:37:22 +0100

    improved draw_repartition

--- draw_repartition.py
+++ draw_repartition.py
@@ -1,15 +1,18 @@
-import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
  
+if len(sys.argv) != 2:
+    print("Enter the name of the file with values.")
+    exit(0)
+
 values = [str(i) for i in range(16)]
 
 count = [0 for i in range(16)]
 
-file1 = "demo_mt.txt"
-file2 = "demo_mt_truncated.txt"
+filename = sys.argv[1]
 
-with open (file1,'r') as f:
+with open(filename,'r') as f:
     for line in f:
         count[int(line)]+=1
 
@@ -18,7 +21,8 @@ y_pos = np.arange(len(values))
  
 plt.bar(y_pos, count, align='center', alpha=0.5)
 plt.xticks(y_pos, values)
-plt.ylabel('Apparitions')
+plt.xlabel('Values')
+plt.ylabel('Count')
 plt.title('Repartition of values')
  
 plt.show()
